2c2590fb65bfc6869a5f24063145c26bc24a719b
kareemYasser20 <karimyasser339@gmail.com>
2019-10-09 20:36:57 +0200

    change1

--- app/src/main/java/com/example/whatsappproject/LoginWithPhone.java
+++ app/src/main/java/com/example/whatsappproject/LoginWithPhone.java
@@ -11,20 +11,27 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
 import com.hbb20.CountryCodePicker;
+
+import java.util.concurrent.TimeUnit;
 
 public class LoginWithPhone extends AppCompatActivity {
 
     private EditText Phonenumber;
     private Button next;
-    String number;
-    private String PhoneVerviectionid;
-    private PhoneAuthProvider.OnVerificationStateChangedCallbacks verificationCallbacks;
-    private PhoneAuthProvider.ForceResendingToken resendingToken;
-    private FirebaseAuth auth;
+//    String number;
+//    private String PhoneVerviectionid;
+////    private PhoneAuthProvider.OnVerificationStateChangedCallbacks verificationCallbacks;
+//    private PhoneAuthProvider.ForceResendingToken resendingToken;
     CountryCodePicker ccp;
+
+  private   FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+    String Codesent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,47 +42,79 @@ public class LoginWithPhone extends AppCompatActivity {
         next=findViewById(R.id.Next_btn);
         ccp=findViewById(R.id.Countery_code);
         ccp.registerCarrierNumberEditText(Phonenumber);
-        auth=FirebaseAuth.getInstance();
         ccp.registerCarrierNumberEditText(Phonenumber);
         ccp.setNumberAutoFormattingEnabled(true);
         ccp.setFullNumber("");
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(LoginWithPhone.this, "the number is" + ccp.getFormattedFullNumber(), Toast.LENGTH_LONG).show();
+              Toast.makeText(LoginWithPhone.this, "the number is" + ccp.getFormattedFullNumber(), Toast.LENGTH_LONG).show();
 
-                ShowConfirmnumberDialog();
+//                ShowConfirmnumberDialog();
+                SendVerifcationcode();
 
             }
         });
     }
 
-    private void ShowConfirmnumberDialog() {
-        final Dialog dialog=new Dialog(this);
-        dialog.setContentView(R.layout.confirm_number_dialog);
-        TextView phonenumber = findViewById(R.id.number_entered);
-        TextView Okbuttton = findViewById(R.id.Ok_choice);
-        Button editbutton = findViewById(R.id.edit_choice);
+    private void SendVerifcationcode() {
 
-            String phone = "+201092665188";
-        SpannableStringBuilder ssb = new SpannableStringBuilder(phone);
-        ssb.clear();
-        ssb.append(ccp.getFormattedFullNumber());
-        Phonenumber.setText(ssb);
+        Toast.makeText(this, "number send" +  ccp.getFormattedFullNumber(), Toast.LENGTH_SHORT).show();
 
-        Toast.makeText(this, "" + Phonenumber.getText().toString(), Toast.LENGTH_SHORT).show();
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                ccp.getFormattedFullNumber(),        // Phone number to verify
+                60,                              // Timeout duration
+                TimeUnit.SECONDS,                  // Unit of timeout
+                this,                      // Activity (for callback binding)
+                mCallbacks);                     // OnVerificationStateChangedCallbacks
 
-        editbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(LoginWithPhone.this, "Edieeeeeeeeeeeeeeeeeeeeeeeettttttttttttt", Toast.LENGTH_SHORT).show();
-                dialog.dismiss();
-            }
-        });
-       dialog.show();
 
 
     }
+
+
+    PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+        @Override
+        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+
+            Toast.makeText(LoginWithPhone.this, "onVerificationCompleted", Toast.LENGTH_SHORT).show();
+
+        }
+
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+
+            Toast.makeText(LoginWithPhone.this, "onVerificationFailed" + e  , Toast.LENGTH_SHORT).show();
+
+        }
+
+        @Override
+        public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+
+            Codesent = s;
+
+            Toast.makeText(LoginWithPhone.this, "Code sent " + Codesent, Toast.LENGTH_SHORT).show();
+
+        }
+    };
+
+//    private void ShowConfirmnumberDialog() {
+//        final Dialog dialog=new Dialog(this);
+//        dialog.setContentView(R.layout.confirm_number_dialog);
+//        TextView phoneenterd = findViewById(R.id.number_entered);
+//        TextView Okbuttton = findViewById(R.id.Ok_choice);
+//        TextView editbutton = findViewById(R.id.edit_choice);
+//
+//        phoneenterd.setText(ccp.getFormattedFullNumber());
+//
+//        Toast.makeText(this, "" + Phonenumber.getText().toString(), Toast.LENGTH_SHORT).show();
+//
+//
+//       dialog.show();
+//
+//
+//    }
 
 
 }
